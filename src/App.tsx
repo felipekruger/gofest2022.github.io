b209dde1e4d44aadaaa18811ed21b2fb50289064
felipe kruger <felipekrugersilveira@live.com>
2022-08-24 21:48:34 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,10 @@ import { Pokemon } from './models/interfaces';
 
 
 function App() {
+  localStorage.clear();
   if(localStorage.getItem("pokemon")===null){
     localStorage.setItem("pokemon", JSON.stringify(allPokemon));
   };
-
-  const pokemonCheck:Pokemon[] = JSON.parse(localStorage.getItem("pokemon") || '');
-
-  if(pokemonCheck.length !== allPokemon.length){
-    localStorage.setItem("pokemon", JSON.stringify(allPokemon));
-  }
 
   const pokemonSave:Pokemon[] = JSON.parse(localStorage.getItem("pokemon") || '');
 
